9bb0251807499b2f5f7318ec8f6db79a97215dcf
bonnier.mamani <bonnier.mamani@lim.upeu.edu.pe>
2023-12-29 23:37:37 -0500

    labs

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import {Component, signal} from '@angular/core';
-import {NgForOf} from "@angular/common";
+import {JsonPipe, NgForOf} from "@angular/common";
 import {Task} from "../../models/task.models";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    JsonPipe
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -16,7 +17,7 @@ export class HomeComponent {
     {
       id: Date.now(),
       title: "Crear Proyecto",
-      completed: true
+      completed: false
     },
     {
       id: Date.now(),
@@ -28,21 +29,36 @@ export class HomeComponent {
   changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
-    this.tasks.update((tasks) => [...tasks, newTask]);
-
+    this.addTask(newTask);
   }
 
+
   addTask(title: string) {
-    const newTasl={
-      id:Date.now(),
+    const newTask = {
+      id: Date.now(),
       title,
-      completed:false
-      
+      completed: false
+
     }
+    this.tasks.update((tasks) => [...tasks, newTask]);
+
 
   }
 
   deleteTask(index: number) {
     this.tasks.update((tasks) => tasks.filter((task, position) => position != index));
+  }
+
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task, completed: !task.completed
+          }
+        }
+        return task
+      })
+    });
   }
 }
